aeb0abed07d385f36a1a5710e27b7796a21935a1
talg2324 <tgrutman@yahoo.com>
2024-04-14 12:14:03 -0400

    jupyter setup

--- Project/setup/setup_jupyter.sh
+++ Project/setup/setup_jupyter.sh
@@ -2,7 +2,7 @@
 
 echo Jupyter setup script ...
 jupyter notebook --generate-config
-cp -f ./jupyter_notebook_config.py ~/.jupyter/
+cp -f ./setup/jupyter_notebook_config.py ~/.jupyter/
 echo Please set a password for your Jupyter notebook
-python -m notebook.auth password
+python -m jupyter_server.auth password
 sudo chmod -R 777 /opt
