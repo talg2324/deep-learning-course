1c73537ce1ed33ccf01314e940a843df040d0ea9
talg <tgrutman@yahoo.com>
2024-04-23 18:07:03 -0400

    move all current params to yaml

--- Project/train_script.sh
+++ Project/train_script.sh
@@ -1,2 +1,9 @@
 cd latent-diffusion
-CUDA_VISIBLE_DEVICES=0 python main.py --base configs/latent-diffusion/ct-rsna.yaml -t --gpus 0, --resume logs/2024-04-18T18-57-41_ct-rsna/checkpoints/last.ckpt
+LD_LIBRARY_PATH=/opt/conda/lib \
+CUDA_VISIBLE_DEVICES=0 \
+python main.py \
+  --base configs/latent-diffusion/ct-rsna.yaml \
+  -t \
+  --gpus 0, \
+  --num_sanity_val_steps 0 \
+#   --resume logs/2024-04-18T18-57-41_ct-rsna/checkpoints/last.ckpt
