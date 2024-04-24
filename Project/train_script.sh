f802e8d4996669019a13411f777a8e5444edaac8
talg <tgrutman@yahoo.com>
2024-04-24 11:31:16 -0400

    echo steps of training prep

--- Project/train_script.sh
+++ Project/train_script.sh
@@ -5,10 +5,12 @@
 echo "Please enter the experiment name:"
 read name
 
+echo "Copying base model..."
 # Copy the directory to use the provided name
 cp -r ./data/outputs/base-model/ ./data/outputs/${name}
 
 ## Training
+echo "Training ${name}..."
 cd latent-diffusion
 LD_LIBRARY_PATH=/opt/conda/lib \
 CUDA_VISIBLE_DEVICES=0 \
