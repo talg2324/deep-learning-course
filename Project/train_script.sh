fe0fdc3562ef7c3d7db531d02e43a46b02ee61b4
talg <tgrutman@yahoo.com>
2024-04-24 11:29:06 -0400

    use base model without redoing each time

--- Project/train_script.sh
+++ Project/train_script.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+## Resume training - comment out to start from scratch
+# Ask the user for an experiment name
+echo "Please enter the experiment name:"
+read name
+
+# Copy the directory to use the provided name
+cp -r ./data/outputs/base-model/ ./data/outputs/${name}
+
+## Training
 cd latent-diffusion
 LD_LIBRARY_PATH=/opt/conda/lib \
 CUDA_VISIBLE_DEVICES=0 \
@@ -7,5 +18,5 @@ python main.py \
   --gpus 0, \
   --max_epochs 70 \
   --num_sanity_val_steps 0 \
-  --logdir ../data/outputs
-#   --resume logs/2024-04-18T18-57-41_ct-rsna/checkpoints/last.ckpt
+  --logdir ../data/outputs \
+  --resume logs/${name}/checkpoints/last.ckpt
