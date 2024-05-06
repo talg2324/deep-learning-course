78b66d8aea03dde1a1414c77f0d7dbaf0a08eeeb
talg <tgrutman@yahoo.com>
2024-05-06 14:58:34 -0400

    print scale factor

--- Project/brats-mri/brats_mri_class_cond/scripts/utils.py
+++ Project/brats-mri/brats_mri_class_cond/scripts/utils.py
@@ -30,6 +30,7 @@ def compute_scale_factor(autoencoder, train_loader, device, fast=True):
                     latent_vectors.append(z.cpu())
                 pbar.set_postfix(scale_factor=f'{scale_factor:.4f}')
         scale_factor = 1. / torch.cat(latent_vectors).std().item()
+    print(f'Scale factor: {scale_factor:.5f}')
     return scale_factor
     
 
